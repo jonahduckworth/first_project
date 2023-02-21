db4e15c35637d76214e3b28ac510812ac5c4afba
jones <35585020+fleeced@users.noreply.github.com>
2023-02-21 16:06:00 -0700

    using mysql instead of sqlite

--- db/db.go
+++ db/db.go
@@ -4,15 +4,15 @@ import (
 	"database/sql"
 	"fmt"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func Connect() (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", "db/mydatabase.db")
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/mydb")
 	if err != nil {
 		return nil, fmt.Errorf("Error opening database: %v", err)
 	}
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT)")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255))")
 	if err != nil {
 		return nil, fmt.Errorf("Error creating table: %v", err)
 	}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.7.0
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.10.0
 	github.com/labstack/gommon v0.4.0 // indirect
