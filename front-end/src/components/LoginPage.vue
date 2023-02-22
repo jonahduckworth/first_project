dec04d8b179e9009919f3b904d0475c8170273c4
jones <35585020+fleeced@users.noreply.github.com>
2023-02-22 15:41:53 -0700

    added some styling to login screen

--- front-end/src/components/LoginPage.vue
+++ front-end/src/components/LoginPage.vue
@@ -1,16 +1,28 @@
 <template>
-    <div>
+    <div class="form-container">
         <h1>Login</h1>
         <form>
-            <div>
+            <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" id="email" v-model="email" />
+                <input
+                    type="email"
+                    id="email"
+                    v-model="email"
+                    class="form-control"
+                />
             </div>
-            <div>
+            <div class="form-group">
                 <label for="password">Password:</label>
-                <input type="password" id="password" v-model="password" />
+                <input
+                    type="password"
+                    id="password"
+                    v-model="password"
+                    class="form-control"
+                />
             </div>
-            <button @click.prevent="submit">Submit</button>
+            <button @click.prevent="submit" class="btn btn-primary">
+                Submit
+            </button>
         </form>
         <p>
             Don't have an account yet?
@@ -18,6 +30,38 @@
         </p>
     </div>
 </template>
+<style scoped>
+.form-container {
+    width: 400px;
+    margin: 0 auto;
+    padding: 40px;
+    background-color: #f7f7f7;
+    border-radius: 10px;
+}
+
+.form-group {
+    margin-bottom: 25px;
+}
+
+.form-control {
+    width: 100%;
+    padding: 10px;
+    font-size: 16px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+
+.btn {
+    width: 100%;
+    padding: 10px;
+    font-size: 16px;
+    border-radius: 5px;
+    background-color: #007bff;
+    color: #fff;
+    border: 0;
+    cursor: pointer;
+}
+</style>
 <script>
 import axios from "axios";
 
