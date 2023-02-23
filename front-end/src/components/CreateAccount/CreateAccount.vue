28799023d3cab51f6934beefc74382f6c8337948
jones <35585020+fleeced@users.noreply.github.com>
2023-02-23 10:03:37 -0700

    made create account look nice and centered both containers

--- front-end/src/components/CreateAccount/CreateAccount.vue
+++ front-end/src/components/CreateAccount/CreateAccount.vue
@@ -1,23 +1,44 @@
 <template>
-    <div>
+    <div class="form-container">
         <h1>Create an Account</h1>
         <form>
-            <div>
+            <div class="form-group">
                 <label for="name">Name:</label>
-                <input type="text" id="name" v-model="name" />
+                <input
+                    type="text"
+                    id="name"
+                    v-model="name"
+                    class="form-control"
+                />
             </div>
-            <div>
+            <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" id="email" v-model="email" />
+                <input
+                    type="email"
+                    id="email"
+                    v-model="email"
+                    class="form-control"
+                />
             </div>
-            <div>
+            <div class="form-group">
                 <label for="password">Password:</label>
-                <input type="password" id="password" v-model="password" />
+                <input
+                    type="password"
+                    id="password"
+                    v-model="password"
+                    class="form-control"
+                />
             </div>
-            <button @click.prevent="submit">Submit</button>
+            <button @click.prevent="submit" class="btn btn-primary">
+                Submit
+            </button>
         </form>
     </div>
 </template>
+
+<style scoped>
+@import "./CreateAccount.css";
+</style>
 
 <script>
 import createAccount from "./CreateAccount.js";
