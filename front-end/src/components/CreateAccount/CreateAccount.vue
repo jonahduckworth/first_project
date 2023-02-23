37e3092dd6f1664de8177684ce8fecbc72fe6ac7
jones <35585020+fleeced@users.noreply.github.com>
2023-02-23 12:03:42 -0700

    added page title

--- front-end/src/components/CreateAccount/CreateAccount.vue
+++ front-end/src/components/CreateAccount/CreateAccount.vue
@@ -1,38 +1,41 @@
 <template>
-    <div class="shadow-container"></div>
-    <div class="form-container">
-        <form>
-            <div class="form-group">
-                <label for="name"> Name:</label>
-                <input
-                    type="text"
-                    id="name"
-                    v-model="name"
-                    class="form-control"
-                />
-            </div>
-            <div class="form-group">
-                <label for="email"> Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    v-model="email"
-                    class="form-control"
-                />
-            </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input
-                    type="password"
-                    id="password"
-                    v-model="password"
-                    class="form-control"
-                />
-            </div>
-            <button @click.prevent="submit" class="btn btn-primary">
-                Create
-            </button>
-        </form>
+    <div>
+        <div class="create-account-text">Create Account</div>
+        <div class="shadow-container"></div>
+        <div class="form-container">
+            <form>
+                <div class="form-group">
+                    <label for="name"> Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        v-model="name"
+                        class="form-control"
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="email"> Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        v-model="email"
+                        class="form-control"
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        v-model="password"
+                        class="form-control"
+                    />
+                </div>
+                <button @click.prevent="submit" class="btn btn-primary">
+                    Create
+                </button>
+            </form>
+        </div>
     </div>
 </template>
 
