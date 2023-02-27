f5f89549d7dc6d9d5683aca3b84df085a294e62a
jones <35585020+fleeced@users.noreply.github.com>
2023-02-27 11:58:07 -0700

    front end handles empty email/password

--- front-end/src/components/LoginPage/LoginPage.vue
+++ front-end/src/components/LoginPage/LoginPage.vue
@@ -11,7 +11,11 @@
                         id="email"
                         v-model="email"
                         class="form-control"
+                        :class="{ 'is-invalid': emailError }"
                     />
+                    <div class="invalid-feedback" v-if="emailError">
+                        Please enter your email.
+                    </div>
                 </div>
                 <div class="form-group">
                     <label for="password">Password:</label>
@@ -20,7 +24,11 @@
                         id="password"
                         v-model="password"
                         class="form-control"
+                        :class="{ 'is-invalid': passwordError }"
                     />
+                    <div class="invalid-feedback" v-if="passwordError">
+                        Please enter your password.
+                    </div>
                 </div>
                 <div style="height: 20px; background-color: white"></div>
                 <button @click.prevent="handleSubmit" class="btn btn-primary">
@@ -33,6 +41,16 @@
 
 <style scoped>
 @import "./LoginPage.css";
+
+.is-invalid {
+    border-color: red;
+}
+
+.invalid-feedback {
+    color: red;
+    font-size: 10px;
+    margin-top: 5px;
+}
 </style>
 
 <script>
@@ -40,8 +58,25 @@ import login from "./Login.js";
 
 export default {
     mixins: [login],
+    data() {
+        return {
+            emailError: false,
+            passwordError: false,
+        };
+    },
     methods: {
         async handleSubmit() {
+            this.emailError = false;
+            this.passwordError = false;
+            if (!this.email) {
+                this.emailError = true;
+            }
+            if (!this.password) {
+                this.passwordError = true;
+            }
+            if (!this.email || !this.password) {
+                return;
+            }
             const response = await this.submit();
             if (response.data.userExists) {
                 this.$router.push({ path: "/dashboard" });
