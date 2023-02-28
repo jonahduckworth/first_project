462b262408bb3057b8a1f5bae04648cf792fab4b
jones <35585020+fleeced@users.noreply.github.com>
2023-02-28 11:37:32 -0700

    dashboard has tiles

--- front-end/src/components/DashboardPage/DashboardPage.vue
+++ front-end/src/components/DashboardPage/DashboardPage.vue
@@ -1,13 +1,33 @@
 <template>
     <div>
-        <div class="dashboard-container">
-            <h1>Welcome to the Dashboard</h1>
+        <div class="container">
+            <div class="container-row">
+                <div class="tile">
+                    <p>Profile</p>
+                </div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile">
+                    <p>Sign Out</p>
+                </div>
+            </div>
+            <div style="height: 15px; background-color: white"></div>
         </div>
     </div>
 </template>
 
 <style scoped>
 @import "@/css/DashboardPage.css";
+
+.container-row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
 </style>
 
 <script>
