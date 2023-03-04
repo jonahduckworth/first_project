ad8b49415f6e0fb4e3f215f9919a682621dacb00
jones <35585020+fleeced@users.noreply.github.com>
2023-03-04 11:28:24 -0700

    added some css to display items nicer

--- front-end/src/components/HomePage/StoreView/StoreView.vue
+++ front-end/src/components/HomePage/StoreView/StoreView.vue
@@ -1,12 +1,59 @@
 <template>
-    <div class="main-container">
-        <div v-for="item in items" :key="item.ID">
-            <p>{{ item.Title }}</p>
-            <p>{{ item.Price }}</p>
-            <img :src="item.ImageUrl" />
+    <div class="store-container">
+        <div class="container-row">
+            <div class="item-container" v-for="item in items" :key="item.ID">
+                <img :src="item.ImageUrl" />
+                <p>{{ item.Title }}</p>
+                <p>{{ item.Price }}</p>
+            </div>
         </div>
     </div>
 </template>
+
+<style>
+.store-container {
+    width: 85%;
+    min-width: 365px;
+    height: 67%;
+    margin: 0 auto;
+    padding: 30px;
+    background-color: white;
+    border-radius: 0;
+    border: 1px solid black;
+    position: absolute;
+    top: 58%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    box-shadow: 15px 15px 0px 0px black;
+}
+
+.container-row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+}
+
+.item-container {
+    width: 100px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0 auto;
+    padding: 30px;
+    background-color: white;
+    border-radius: 0;
+    border: 1px solid black;
+    box-shadow: 5px 5px 0px 0px black;
+}
+
+img {
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+    margin-bottom: 20px;
+}
+</style>
 
 <script>
 import { getItems } from "./GetItems";
